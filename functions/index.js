a2913513eb342fe107c41f16f054b7df5b6b18e0
YenLap <s810643@gmail.com>
2019-10-15 00:07:58 +0800

    Add Store functionality
    增加新增店家功能
    db_store.js 裡面有Add方法
    使用 body-parser 解析ajax傳回物件

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,54 @@
-const functions = require("firebase-functions");
+﻿const functions = require("firebase-functions");
 const express = require("express");
 
 const app = express();
 const path = require("path");
 
+var bodyParser = require('body-parser')
+
 app.get("/", (req, res) => {
-  res.send("Root page!");
+    res.send("Root page!");
 });
 
 app.get("/store", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/store.html"));
+    res.sendFile(path.join(__dirname + "/views/store.html"));
 });
 
 const db_store = require("./js/db_store.js");
-app.get("/api/getStoreInfo", function(req, res) {
-  db_store
-    .getStoreInfo()
-    .then(result => {
-      res.json(result.recordsets);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Server error" + err });
-    });
+app.get("/api/getstore", function (req, res) {
+    db_store
+        .getStore()
+        .then(result => {
+            res.json(result.recordsets);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Server error" + err });
+        });
 });
+
+//app.post("/api/addStore", function (req, res) {
+//    db_store
+//        .addStore()
+//        .then(result => {
+//            res.json(result.recordsets);
+//        })
+//        .catch(err => {
+//            res.status(500).json({ message: "Server error" + err });
+//        });
+//});
+
+app.post('/api/addStore', function (req, res) {
+    console.dir("收到");
+    console.dir(req.body);
+    db_store
+        .addStore(req.body)
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Server error" + err });
+        });
+})
 
 // app.get("/timestamp", (req, res) => {
 //   var time = helper.f2().toString();
--- functions/js/db_store.js
+++ functions/js/db_store.js
@@ -2,27 +2,60 @@ const db = require("./db.js");
 const sql = require("mssql");
 
 const query = () => {
-  return new Promise((resolve, reject) => {
-    sql.connect(db.config, function(err) {
-      if (err) console.log(err);
+    return new Promise((resolve, reject) => {
+        sql.connect(db.config, function (err) {
+            if (err) console.log(err);
 
-      // create Request object
-      var request = new sql.Request();
-      var command = "select * from StoreInfo";
+            // create Request object
+            var request = new sql.Request();
+            var command = "select * from StoreInfo";
 
-      // query to the database and get the records
-      request.query(command, function(err, recordset) {
-        if (err) {
-          console.dir(err);
-          reject(err);
-        } else {
-          resolve(recordset);
-        }
-      });
+            // query to the database and get the records
+            request.query(command, function (err, recordset) {
+                if (err) {
+                    console.dir(err);
+                    reject(err);
+                } else {
+                    resolve(recordset);
+                }
+            });
+        });
     });
-  });
+};
+
+const add = (store) => {
+    return new Promise((resolve, reject) => {
+        console.dir("===========db_store===========");
+        console.dir(store);
+        sql.connect(db.config, function (err) {
+            if (err) console.log(err);
+
+            // create Request object
+            var request = new sql.Request();
+            var command = "insert into StoreInfo([Name],[Address],[Phone],[Remark])"
+            command += "values("
+            command += "'" + store.name + "', "
+            command += "'" + store.address + "', "
+            command += "'" + store.phone + "', "
+            command += "'" + store.remark + "'"
+            command += ")";
+
+            // query to the database and get the records
+            request.query(command, function (err, result) {
+                if (err) {
+                    console.dir(err);
+                    reject(err);
+                } else {
+                    console.dir("===========Success===========");
+                    console.dir(result.rowsAffected);
+                    resolve(result);
+                }
+            });
+        });
+    });
 };
 
 module.exports = {
-  getStoreInfo: query
+    getStore: query,
+    addStore: add
 };
