b510a79761533a5f8ccbe7a726bf5154b6c665ae
YenLap <s810643@gmail.com>
2019-10-21 23:43:30 +0800

    店家基本資訊編輯
    目前店家基本資訊可以編輯跟新增

--- functions/js/db_store.js
+++ functions/js/db_store.js
@@ -32,14 +32,13 @@ const add = (store) => {
 
             // create Request object
             var request = new sql.Request();
-            var command = "insert into StoreInfo([Name],[Address],[Phone],[Remark])"
+            var command = "insert into StoreInfo([Name],[Address],[Phone])"
             command += "values("
             command += "'" + store.name + "', "
             command += "'" + store.address + "', "
-            command += "'" + store.phone + "', "
-            command += "'" + store.remark + "'"
-            command += ")";
-
+            command += "'" + store.phone + "'"
+            command += ") SELECT SCOPE_IDENTITY() as sid";
+            console.dir("insert command: " + command);
             // query to the database and get the records
             request.query(command, function (err, result) {
                 if (err) {
@@ -68,10 +67,9 @@ const update = (store) => {
             command += " Name=N'" + store.name + "',"
             command += " Address=N'" + store.address + "',"
             command += " Phone=N'" + store.phone + "',"
-            command += " Remark=N'" + store.remark + "',"
             command += " UpdatedDate=GetDate()"
-            command += " WHERE SID=" + store.SID;
-
+            command += " WHERE SID=" + store.sid;
+            console.dir("update command: "+command);
             // query to the database and get the records
             request.query(command, function (err, result) {
                 if (err) {
